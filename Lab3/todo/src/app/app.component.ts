f3328ad76273947970ee5f7e77d2726503171c46
CherpakAndrii <a.cherpak-2026@kpi.ua>
2023-10-20 02:25:23 +0300

    Create a base of todo app

--- Lab3/todo/src/app/app.component.ts
+++ Lab3/todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from "./todoList";
+import { TodoItem } from "./todoItem";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList("Андрій", [
+    new TodoItem("Зробити пробіжку", true),
+    new TodoItem("Купити квіти"),
+    new TodoItem("Забрати квитки"),
+  ]);
+  get username(): string {
+    return this.list.user;
+  }
+  get itemCount(): number {
+    return this.list.items
+      .filter(item => !item.complete).length;
+  }
 }
